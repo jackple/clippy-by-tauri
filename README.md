42f61561d865b108b0c42cb260c99a9fa83b662a
商海涛 <shanghaitao@ydjdev.com>
2025-01-17 11:57:32 +0800

    feat: 代码签名 + icon修改 ...

--- README.md
+++ README.md
@@ -5,3 +5,17 @@ This template should help get you started developing with Tauri, React and Types
 ## Recommended IDE Setup
 
 - [VS Code](https://code.visualstudio.com/) + [Tauri](https://marketplace.visualstudio.com/items?itemName=tauri-apps.tauri-vscode) + [rust-analyzer](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer)
+
+# 构建 icon
+
+前提是当前目录下有`app-icon.png`文件
+
+```sh
+yarn tauri icon
+```
+
+# 构建 mac universal 包
+
+```sh
+yarn tauri build --target universal-apple-darwin
+```
--- src-tauri/src/utils/global_shortcut.rs
+++ src-tauri/src/utils/global_shortcut.rs
@@ -6,12 +6,12 @@ use tauri_plugin_global_shortcut::{Code, Modifiers, ShortcutState};
 pub fn register(app: &tauri::App) {
     let _ = app.app_handle().plugin(
         tauri_plugin_global_shortcut::Builder::new()
-            .with_shortcut("CommandOrControl+Shift+B")
+            .with_shortcut("CommandOrControl+Shift+V")
             .unwrap()
             .with_handler(|app_handle, shortcut, event| {
                 if event.state == ShortcutState::Pressed {
                     // println!("shortcut: {:#?}", shortcut);
-                    if shortcut.matches(Modifiers::SHIFT | Modifiers::SUPER, Code::KeyB) {
+                    if shortcut.matches(Modifiers::SHIFT | Modifiers::SUPER, Code::KeyV) {
                         nspanel::toggle_panel(app_handle.to_owned());
                     }
                 }
