74108e7518c0ef8e459edcd2860f3e2791548c91
商海涛 <shanghaitao@ydjdev.com>
2025-01-20 16:37:38 +0800

    fix: 单例下隐藏dock icon

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -5,7 +5,9 @@ mod utils;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_single_instance::init(|_app, _args, _cwd| {}))
+        .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            let _ = app.set_activation_policy(tauri::ActivationPolicy::Accessory);
+        }))
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_nspanel::init())
