ad5687f849242986ad999bf4447b09afec338f8a
商海涛 <shanghaitao@ydjdev.com>
2025-02-08 11:52:06 +0800

    feat: 使用内置的about和quit菜单项

--- src-tauri/src/utils/tray.rs
+++ src-tauri/src/utils/tray.rs
@@ -3,17 +3,17 @@ use tauri::path::BaseDirectory;
 use tauri::Manager;
 use tauri::{
     image,
-    menu::{Menu, MenuItem, PredefinedMenuItem},
+    menu::{AboutMetadata, Menu, MenuItem, PredefinedMenuItem},
     tray::TrayIconBuilder,
 };
-use tauri_plugin_dialog::DialogExt;
 
 pub fn init(app: &tauri::App) {
     let separator = PredefinedMenuItem::separator(app).unwrap();
     let open = MenuItem::with_id(app, "open", "打开面板", true, Some("Cmd+Shift+V")).unwrap();
-    let about = MenuItem::with_id(app, "about", "关于clippy2", true, None::<&str>).unwrap();
-    let quit = MenuItem::with_id(app, "quit", "退出clippy2", true, None::<&str>).unwrap();
-    let menu = Menu::with_items(app, &[&open, &about, &separator, &quit]).unwrap();
+    let about =
+        PredefinedMenuItem::about(app, Some("关于clippy2"), Some(generate_metadata(&app))).unwrap();
+    let quit = PredefinedMenuItem::quit(app, Some("退出clippy2")).unwrap();
+    let menu = Menu::with_items(app, &[&open, &separator, &about, &quit]).unwrap();
 
     let icon = image::Image::from_path(
         app.path()
@@ -31,18 +31,6 @@ pub fn init(app: &tauri::App) {
                 println!("open panel by tray");
                 nspanel::toggle_panel(app.clone());
             }
-            "about" => {
-                let pkg_info = app.package_info();
-                let message = format!("版本: {}\n作者: {}", pkg_info.version, pkg_info.authors);
-                app.dialog()
-                    .message(message)
-                    .title("关于clippy2")
-                    .blocking_show();
-            }
-            "quit" => {
-                println!("quit menu item was clicked");
-                app.exit(0);
-            }
             _ => {
                 println!("menu item {:?} not handled", event.id);
             }
@@ -50,4 +38,30 @@ pub fn init(app: &tauri::App) {
         .tooltip("Cmd+Shift+V 打开")
         .build(app)
         .unwrap();
+}
+
+fn generate_metadata(app: &tauri::App) -> AboutMetadata {
+    let icon = image::Image::from_path(
+        app.path()
+            .resolve("assets/icon.png", BaseDirectory::Resource)
+            .unwrap(),
+    )
+    .unwrap();
+
+    let pkg_info = app.package_info();
+    let metadata = AboutMetadata {
+        name: Some(pkg_info.name.clone()),
+        authors: Some(vec![pkg_info.authors.to_string()]),
+        version: Some(pkg_info.version.to_string()),
+        short_version: None,
+        comments: None,
+        copyright: Some(String::from("Copyright © 2025 yidejia")),
+        license: None,
+        website: None,
+        website_label: None,
+        credits: None,
+        icon: Some(icon),
+    };
+
+    metadata
 }
