7bbb1425ed3f33dfb2fe98156e085efbf91b8de7
商海涛 <shanghaitao@ydjdev.com>
2025-02-08 08:39:19 +0800

    chore: update 脚本

--- update/update.sh
+++ update/update.sh
@@ -13,8 +13,11 @@ sig=$(cat src-tauri/target/universal-apple-darwin/release/bundle/macos/clippy2.a
 sed -i '' "s/\"signature\": \".*\"/\"signature\": \"$sig\"/" update/updater.json
 
 tool=./update/ossutilmac64
+remotePath=oss://yim-sft-download/clippy2/
+configFile="--config-file ~/.yim.ossutilconfig"
+meta="--meta Cache-Control:no-store"
 
-$tool cp -r -u src-tauri/target/universal-apple-darwin/release/bundle/dmg/ oss://yim-sft-download/clippy2/ --config-file ~/.yim.ossutilconfig --include *.dmg
-$tool cp -r -u src-tauri/target/universal-apple-darwin/release/bundle/macos/clippy2.app.tar.gz oss://yim-sft-download/clippy2/ --config-file ~/.yim.ossutilconfig
-$tool cp -r -u update/updater.json  oss://yim-sft-download/clippy2/ --config-file ~/.yim.ossutilconfig
+$tool cp -r -u src-tauri/target/universal-apple-darwin/release/bundle/dmg/ $remotePath $configFile --include *.dmg
+$tool cp -r -u src-tauri/target/universal-apple-darwin/release/bundle/macos/clippy2.app.tar.gz $remotePath $meta $configFile
+$tool cp -r -u update/updater.json $remotePath $meta $configFile
 
